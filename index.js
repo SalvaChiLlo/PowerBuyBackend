88f26535a4b53f47e930c4ebbadfdad15029d2ed
Salva <salvachll@gmail.com>
2021-10-03 01:21:59 +0200

    Deshabilitar actualización de modelos

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function startServer() {
   });
 }
 
-sqldb.sequelize.sync({ force: true })
+sqldb.sequelize.sync()
   .then(startServer)
   .catch((err) => {
     console.error(`Server failed to start due to error: ${err}`);
