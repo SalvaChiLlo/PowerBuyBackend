f34e425c39b867ba98fc42260ccb2ecc8c9181e1
SalvaChLl <salvachll@gmail.com>
2021-11-04 22:15:20 +0100

    preparing for deploy

--- config/environment/development.js
+++ config/environment/development.js
@@ -1,13 +1,25 @@
 require('dotenv').config();
 
 module.exports = {
-  sequelize: {
-    username: 'root',
-    password: 'adminadmin',
-    database: 'sequelize_database_development',
-    host: 'omv.local',
-    dialect: 'mysql',
-    logging: console.log,
+  // Server IP
+  ip: process.env.PRODUCTION_IP || process.env.IP || undefined,
+
+  // Server port
+  port: process.env.PRODUCTION_PORT || process.env.PORT || 9000,
+
+  sequelize:
+  {
+    username: process.env.PRODUCTION_USERNAME,
+    password: process.env.PRODUCTION_PASSWORD,
+    database: process.env.PRODUCTION_DATABASE,
+    host: process.env.PRODUCTION_DB_HOST,
+    dialect: process.env.PRODUCTION_DIALECT,
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
-  seedDB: false,
 };
